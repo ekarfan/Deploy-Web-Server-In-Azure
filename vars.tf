18ddd4655af5baabfdea11e0208c4ed565798ad4
kf3527 <kf3527@att.com>
2021-11-22 14:59:47 -0600

    updated var file

--- vars.tf
+++ vars.tf
@@ -19,13 +19,13 @@ variable "tags" {
 
 variable "username" {
   description = "Enter username to associate with the machine"
-  default     = "jiannshyng.heh@gmail.com"
+  default     = "user"
 
 }
 
 variable "password" {
   description = "Enter password to use to access the machine"
-  default     = "Fan650728"
+  default     = "pwd"
 
 }
 
